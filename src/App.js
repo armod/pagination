896552a0ff656fede8d6302082bdaea08ad39d38
armod <armods@gmail.com>
2022-10-23 22:22:53 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import React, { useState, useEffect } from 'react'
+import { useFetch } from './useFetch'
+import Follower from './Follower'
+
 function App() {
   return <div className='App'></div>
 }
